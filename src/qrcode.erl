a8cec111cf6fbbfce882bf5f74f963e27f2309d2
Steve Forkin <steve.forkin@gmail.com>
2017-02-08 19:33:54 +1100

    added the otp modules to the list that need to be compiled

--- src/qrcode.erl
+++ src/qrcode.erl
@@ -13,47 +13,89 @@
 % limitations under the License.
 
 -module(qrcode).
--behaviour(gen_fsm).
-
 -include("qrcode.hrl").
 -include("qrcode_params.hrl").
 
--export([start_link/0]).
+-behaviour(gen_server).
+
+% interface calls
+-export([start/1, stop/0]).
+    
+% gen_server callbacks
+-export([init/1,
+         handle_call/3, 
+         handle_cast/2,
+         handle_info/2, 
+         terminate/2, 
+         code_change/3]).
+
+%%====================================================================
+%% Server interface
+%%====================================================================
+%% Booting server (and linking to it)
+start(Pars) -> 
+    io:format("Starting with ~p~n",[Pars]),
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [Pars], []).
+
+%% Stopping server asynchronously
+stop() ->
+    io:format("Stopping~n"),
+    gen_server:cast(?MODULE, shutdown).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+init([Pars]) ->
+    io:format("Initializing with ~p~n",[Pars]),
+    process_flag(trap_exit, true),
+    {ok, initialized}.
+
+%% Synchronous, possible return values  
+% {reply,Reply,NewState} 
+% {reply,Reply,NewState,Timeout}
+% {reply,Reply,NewState,hibernate}
+% {noreply,NewState}
+% {noreply,NewState,Timeout}
+% {noreply,NewState,hibernate}
+% {stop,Reason,Reply,NewState} 
+% {stop,Reason,NewState}
+handle_call(Message, From, State) -> 
+    io:format("Generic call handler: '~p' from '~p' while in '~p'~n",[Message, From, State]),
+    {reply, ok, State}.
+
+%% Asynchronous, possible return values
+% {noreply,NewState} 
+% {noreply,NewState,Timeout}
+% {noreply,NewState,hibernate}
+% {stop,Reason,NewState}
+%% normal termination clause
+handle_cast(shutdown, State) ->
+    io:format("Generic cast handler: *shutdown* while in '~p'~n",[State]),
+    {stop, normal, State};
+%% generic async handler
+handle_cast(Message, State) ->
+    io:format("Generic cast handler: '~p' while in '~p'~n",[Message, State]),
+    {noreply, State}.
+
+%% Informative calls
+% {noreply,NewState} 
+% {noreply,NewState,Timeout} 
+% {noreply,NewState,hibernate}
+% {stop,Reason,NewState}
+handle_info(_Message, _Server) -> 
+    io:format("Generic info handler: '~p' '~p'~n",[_Message, _Server]),
+    {noreply, _Server}.
+
+%% Server termination
+terminate(_Reason, _Server) -> 
+    io:format("Generic termination handler: '~p' '~p'~n",[_Reason, _Server]).
+
+
+%% Code change
+code_change(_OldVersion, _Server, _Extra) -> {ok, _Server}.
+
 
 -export([encode/1, encode/2, decode/1]).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%
-% GEN_FSM exported events 
-% setup/teardown/upgrade
-%%%%%%%%%%%%%%%%%%%%%%%%%
--export([init/1, terminate/3, code_change/4, 
-         handle_event/3, handle_sync_event/4, handle_info/3]). 
-
-start_link() ->
-	gen_fsm:start_link(?MODULE, [], []).
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% GEN_FSM CALLBACKS %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%
-
-init([]) ->
-	ok.
-
-terminate(_Reason, _StateName, _State) ->
-	ok.
-
-code_change(_OldVsn, StateName, State, _Extra) ->
-	{ok, StateName, State}.
-
-handle_info(_Event, StateName, State) ->
-	{next_state, StateName, State}.
-
-handle_event(_Event, StateName, State) ->
-	{next_state, StateName, State}.
-
-handle_sync_event(_Event, _From, StateName, State) ->
-	{next_state, StateName, State}.
 
 %%
 decode(_Bin) ->
