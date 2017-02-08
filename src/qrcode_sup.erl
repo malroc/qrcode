b43819e36aad32670bf89899f2a00e1525fde0a1
Steve Forkin <steve.forkin@gmail.com>
2017-02-08 18:04:53 +1100

    added the otp modules to the list that need to be compiled

--- src/qrcode_sup.erl
+++ src/qrcode_sup.erl
@@ -8,6 +8,7 @@ start_link() ->
     supervisor:start_link(?MODULE, []).
 
 init([]) ->
+    AChild = {?SERVER, {stateful,start_link,[]}, permanent, 2000, worker, [qrcode]}
     {ok, {{one_for_one, 1, 5},
-            [qrcode]
+            [AChild]
     }}.
